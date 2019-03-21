95acf1667ac93eb93e9e54023a5d0780d754f0b8
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-21 15:48:01 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,6 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
+  attr_accessible :emails
   def initialize(emails)
     @emails = emails
   end
